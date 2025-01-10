e40a73707eb35f0997116c2df80e1d260220796d
DabAZ <dabaz@dabaz.me>
2025-01-11 02:55:38 +0800

    Add docs

--- app/_meta.global.ts
+++ app/_meta.global.ts
@@ -1,3 +1,18 @@
 export default {
-  "index": "Introduction",
+  index: {
+    type: 'page',
+    display: 'hidden'
+  },
+  docs: {
+    type: 'page',
+    title: '文档',
+    items: {
+      index: '简介',
+      models: {
+        items: {
+          blp: '贝-拉模型',
+        }
+      }
+    }
+  },
 }
