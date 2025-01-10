07665d7643a4229dd6c0019aa99fd12b57b721f5
DabAZ <dabaz@dabaz.me>
2025-01-11 03:14:43 +0800

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ export const metadata: Metadata = {
 
 const IndexPage: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold mb-4">InfoSec Docs by DabAZ</h1>
-      <p className="text-lg mb-8">
-        This is a collection of information security documentation by DabAZ.
-      </p>
+    <div className="flex flex-col items-center justify-center text-center min-h-screen py-2">
+      <h1 className="text-[4rem] sm:text-[6rem] md:text-[8rem] font-extrabold mb-4">
+        信息系统安全
+      </h1>
+      <a href='/docs' className="text-lg sm:text-xl md:text-2xl mb-8 text-blue-600 dark:text-blue-500 underline hover:no-underline transition-opacity duration-300">
+        开始学习 ↗
+      </a>
     </div>
   )
 }
