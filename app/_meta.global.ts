08fd8773b587d1fff2bda1a1c7d5f7681d160d9f
DabAZ <dabaz@dabaz.me>
2025-01-10 23:07:45 +0800

    Update _meta

--- app/_meta.global.ts
+++ app/_meta.global.ts
@@ -3,4 +3,17 @@ export default {
     type: 'page',
     display: 'hidden'
   },
+  docs: {
+    type: 'page',
+    title: 'Docs',
+    items: {
+      index: 'Introduction',
+    }
+  },
+  contact: {
+    type: 'link',
+    title: '联系 ↗',
+    display: 'normal',
+    href: 'mailto:hi@dabaz.me'
+  }
 }
